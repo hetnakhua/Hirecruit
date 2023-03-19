ab47fce72e8d1835feac1ed4a82d83624f3f9fb6
hetnakhua <hetsnakhua@gmail.com>
2023-03-19 07:57:06 +0530

    basic bookmark

--- lib/ui/screens/bookmark.dart
+++ lib/ui/screens/bookmark.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hirecruit/constants/color.dart';
 import 'package:hirecruit/ui/widgets/navbar.dart';
 
 class BookmarkScreen extends StatefulWidget {
@@ -11,7 +13,17 @@ class BookmarkScreen extends StatefulWidget {
 class _BookmarkScreenState extends State<BookmarkScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        // leading: BackButton(color: Colors.black),
+        title: Text(
+          'Bookmark',
+          style: GoogleFonts.lato(color: black, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+      ),
       body: Text('bookmark'),
       bottomNavigationBar: BottomNavBar(),
     );
